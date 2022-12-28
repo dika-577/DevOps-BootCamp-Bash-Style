912aec35ae02288e6de81938a55722484bbfb868
dimitri <korgalidzed@gmail.com>
2022-12-28 18:00:23 +0400

    Second version

--- game.sh
+++ game.sh
@@ -1,52 +1,50 @@
 #!/bin/bash
 
-
-
-echo -e "\n NumberJack \n"
+echo -e "\nNumberJack\n"
 ch=0
-while [ "$ch" -ne 3 ]; do
-    echo  "  
+while [ "$ch" != "3" ]
+do
+    echo "
          PLAY : Hit 1 and enter.
          HELP : Hit 2 and enter.
          EXIT : Hit 3 and enter.
          "
-done
-    read -p -r "Enter your choice :"  ch 
-    if [ "$ch" -eq 1 ];then
-    x=0 ;c=0 ;p=0
-    read -p -r "Enter any number between 0 and 9 :" c 
+    read -r -p "Enter your choice :" ch 
+    if [ "$ch" -eq 1 ]; then
+        x=0 ;c=0 ;p=0
+        read -r -p "Enter any number between 0 and 9 :" c 
     fi
-    while [ "$c" -eq 0 ]
+    while [ "$c" != "0" ]
     do
         x=11 
-     r=$(("shuf -i 0-9 -n 10"))
-        echo "${r[*]}"
+        r=$(shuf -i 0-9 -n 10)
+        echo "$r"
+       break
     done 
     for i in {1..10}; do
-            a[$i]=$i
+        a[$i]=$i
+        echo -e "${a[*]} " 
     done
-        echo "${a[*]} "
-        read -t 5 -p -r "Enter the index of your number : " p
- 
-    if [[ $? -gt 128 ]]; then
-    c=1
-
+    read -r -t 5 -p "Enter the index of your number: " p
+    echo "$?"
+ #   echo "$r[$(($x-1))]"
+    echo "$c -1"
+    echo "Dika"
+    echo "$r"
+    if [[ "$?" -gt 128 ]]; then
+        c=1
     fi
-
-if [ "${r[$(($x))-1]}" -eq "$p" ];then 
-            echo "Great"
-    ((p=p+1))
+    if [ "${r[x-1]}" -eq "$p" ]; then 
+        echo "Great"
+        p=$((p+1))
     else
-            c=1
-
-fi
-
-    if [ "$ch" -eq 2 ];then
-        echo "HELP: INSTRUCTIONS TO PLAY THE GAME. "
-
-
+        c=1
     fi
-if [ "$c" -eq 1 ];then
-            echo -e "\nGAME OVER\n"
-            echo "You scored $p points"
-fi
+    if [ "$ch" -eq 2 ]; then
+        echo "HELP: INSTRUCTIONS TO PLAY THE GAME."
+    fi
+    if [ "$c" -eq 1 ]; then
+        echo -e "\nGAME OVER\n"
+        echo "You scored $p points"
+    fi
+done
